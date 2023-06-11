85237989b59b7a3ed63928fec9d4b367af0ae5b8
whzd <whzdxl@gmail.com>
2023-06-11 22:50:08 +0100

    webdev js intro strings capitalization

--- webdev/complete_2023_web_development_bootcamp/javascript/introduction/strings.js
+++ webdev/complete_2023_web_development_bootcamp/javascript/introduction/strings.js
@@ -2,10 +2,17 @@ const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
-readline.question('What is your message?\n', text => {
-    // strings slice
-    console.log("Your message:\n" + text.slice(0,140))
-    // strings length
-    console.log('You have written ' + text.length + 'characters, your limit is 140 characters.');
+
+readline.question("What is you name?\n", text => {
+    // strings capitalization 
+    console.log("Hello " + text.slice(0,1).toUpperCase() + text.slice(1,text.length).toLowerCase())
     readline.close();
 });
+
+//readline.question('What is your message?\n', text => {
+//    // strings slice
+//    console.log("Your message:\n" + text.slice(0,140))
+//    // strings length
+//    console.log('You have written ' + text.length + 'characters, your limit is 140 characters.');
+//    readline.close();
+//});
