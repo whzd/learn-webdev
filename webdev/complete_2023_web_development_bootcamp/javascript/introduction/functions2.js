3a2ba3e34c4fbe85b3699cba21248c9a1e76eff7
whzd <whzdxl@gmail.com>
2023-06-17 23:36:16 +0100

    webdev js intro functions part2

---
+++ webdev/complete_2023_web_development_bootcamp/javascript/introduction/functions2.js
@@ -0,0 +1,24 @@
+function getMilk(money) {
+    const bottlePrice = 1.5;
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("buy " +  Math.floor(money/bottlePrice) + " bottles of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+getMilk(5);
