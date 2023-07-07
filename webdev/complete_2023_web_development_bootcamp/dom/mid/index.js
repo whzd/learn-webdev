db240212c4fb1834f3bd94d84169f54c6666b77d
whzd <whzdxl@gmail.com>
2023-07-08 00:10:56 +0100

    webdev dom mid atribute manipulation

--- webdev/complete_2023_web_development_bootcamp/dom/mid/index.js
+++ webdev/complete_2023_web_development_bootcamp/dom/mid/index.js
@@ -1,3 +1,4 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Not Third"
 document.querySelector("li a").style.color = "red";
 document.querySelector("h1").classList.toggle("huge");
+document.querySelector("a").setAttribute("href", "http://www.bing.com");
