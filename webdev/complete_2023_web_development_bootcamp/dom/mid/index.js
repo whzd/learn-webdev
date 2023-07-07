6248c19a21281da1fd76c99428a152ae407a3cf5
whzd <whzdxl@gmail.com>
2023-07-08 00:06:29 +0100

    webdev dom mid class manipulation

--- webdev/complete_2023_web_development_bootcamp/dom/mid/index.js
+++ webdev/complete_2023_web_development_bootcamp/dom/mid/index.js
@@ -1,2 +1,3 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Not Third"
 document.querySelector("li a").style.color = "red";
+document.querySelector("h1").classList.toggle("huge");
