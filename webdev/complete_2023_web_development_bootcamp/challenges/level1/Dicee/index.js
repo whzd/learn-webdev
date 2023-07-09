da710d02ab6bb317deb97f1fb8c6801b172e12c0
whzd <whzdxl@gmail.com>
2023-07-09 23:19:53 +0100

    webdev challenge1 dice game

---
+++ webdev/complete_2023_web_development_bootcamp/challenges/level1/Dicee/index.js
@@ -0,0 +1,12 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1)
+document.getElementsByClassName("img1")[0].setAttribute("src","./images/dice" + randomNumber1 + ".png");
+var randomNumber2 = Math.floor((Math.random()*6)+1)
+document.getElementsByClassName("img2")[0].setAttribute("src","./images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerText = "Draw!"
+}else if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerText = "🚩 Player1 Wins!"
+}else{
+    document.querySelector("h1").innerText = "Player2 Wins! 🚩"
+}
