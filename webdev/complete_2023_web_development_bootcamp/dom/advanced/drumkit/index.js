dc1185457742d00ada8e6bd09c2a95959a643a22
whzd <whzdxl@gmail.com>
2023-07-11 22:35:49 +0100

    webdev js advanced play audio, keyword this

--- webdev/complete_2023_web_development_bootcamp/dom/advanced/drumkit/index.js
+++ webdev/complete_2023_web_development_bootcamp/dom/advanced/drumkit/index.js
@@ -1,6 +1,8 @@
 let buttons = document.querySelectorAll(".drum");
+var audio = new Audio("./sounds/tom-1.mp3");
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", function(){
-        alert("I got clicked!");
+        audio.play();
+        this.style.color = "white";
     });
 }
