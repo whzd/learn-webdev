0ba2a1a20e6b3dae8ffbb08c0dacb2be2d8e2379
whzd <whzdxl@gmail.com>
2023-07-12 23:34:37 +0100

    webdev js advanced JS Objects and constructor

---
+++ webdev/complete_2023_web_development_bootcamp/challenges/random/housekeeper.js
@@ -0,0 +1,23 @@
+ var housekeeper1 = {
+     name: "Concuela",
+     age: 23,
+     hasWorkPermit: true,
+     yearOfExperience: 5,
+     favouriteCleaningTasks: ["laundry", "dishes"]
+ }
+
+console.log(housekeeper1.name);
+
+class HouseKeeper {
+    constructor(name, age, hasWorkPermit, yearOfExperience, favouriteCleaningTasks) {
+        this.name = name;
+        this.age = age;
+        this.hasWorkPermit = hasWorkPermit;
+        this.yearOfExperience = yearOfExperience;
+        this.favouriteCleaningTasks = favouriteCleaningTasks;
+    }
+}
+
+var housekeeper2 = new HouseKeeper("Maria", 24, true, 6, ["toilet", "bedroom"]);
+
+console.log(housekeeper2.name);
