e42ad7220f624a870943f9afcba4e51c4fa8714d
whzd <whzdxl@gmail.com>
2023-07-13 22:19:50 +0100

    webdev js advanced switch statement

--- webdev/complete_2023_web_development_bootcamp/dom/advanced/drumkit/index.js
+++ webdev/complete_2023_web_development_bootcamp/dom/advanced/drumkit/index.js
@@ -1,8 +1,40 @@
 let buttons = document.querySelectorAll(".drum");
-var audio = new Audio("./sounds/tom-1.mp3");
 for (let index = 0; index < buttons.length; index++) {
-    buttons[index].addEventListener("click", function(){
-        audio.play();
-        this.style.color = "white";
+    buttons[index].addEventListener("click", function () {
+
+        switch (this.innerText) {
+            case "w":
+                var tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("./sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+            case "k":
+                var kickbass = new Audio("./sounds/kick-bass.mp3");
+                kickbass.play();
+                break;
+            case "l":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+
+            default:
+                console.log(this.innerText);
+                break;
+        }
     });
 }
