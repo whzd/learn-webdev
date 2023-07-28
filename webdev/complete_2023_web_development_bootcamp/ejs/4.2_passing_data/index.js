bd54290d46d17b4af1b070b3512e2bf79b952fbe
whzd <whzdxl@gmail.com>
2023-07-28 23:13:11 +0100

    webdev ejs passing data to ejs template

---
+++ webdev/complete_2023_web_development_bootcamp/ejs/4.2_passing_data/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+    var nrLetters = req.body['fName'].length + req.body['lName'].length
+    res.render("index.ejs", {letters: nrLetters});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
