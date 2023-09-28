052984b761ab2f8d0b7d3c84ff2df2aab53b5905
whzd <whzdxl@gmail.com>
2023-09-28 23:51:58 +0100

    webdev api requests with axios

---
+++ webdev/complete_2023_web_development_bootcamp/apis/5.3_axios/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`);
+    var result = response.data;
+    if (result.length > 1){
+        result = result[Math.floor(Math.random() * result.length)]
+    }
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
