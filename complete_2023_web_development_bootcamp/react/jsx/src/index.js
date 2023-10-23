bfb39aec09bceb6f5dbff7b06c02b3d5261f2ea9
whzd <whzdxl@gmail.com>
2023-10-23 12:11:23 +0100

    introduction to jsx

---
+++ complete_2023_web_development_bootcamp/react/jsx/src/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+    <ul>
+      <li>element1</li>
+      <li>element2</li>
+      <li>element3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+)
