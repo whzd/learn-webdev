3c8dda097887ec618eeff8347d35d96371e26b78
whzd <whzdxl@gmail.com>
2023-10-23 12:21:30 +0100

    js expressions in jsx

--- complete_2023_web_development_bootcamp/react/jsx/src/index.js
+++ complete_2023_web_development_bootcamp/react/jsx/src/index.js
@@ -1,15 +1,20 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+const title = "Eng."
+const name = "whzd"
+
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <h1>Hello {`${title} ${name}`}!</h1>
+    <p>Your lucky number is {Math.floor(Math.random() * 20 + 1)}.</p>
     <ul>
       <li>element1</li>
       <li>element2</li>
       <li>element3</li>
     </ul>
+    <p> Created by {name}.</p>
+    <p> Copyright {new Date().getFullYear()}.</p>
   </div>,
   document.getElementById("root")
 )
