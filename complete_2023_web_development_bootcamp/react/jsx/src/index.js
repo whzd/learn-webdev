4cd5c2b69388a431ccfdcfc6f3546001330721ba
Andre Dias <andrefilipe.dias@farfetch.com>
2023-10-24 22:40:56 +0100

    jsx attributes and styling

--- complete_2023_web_development_bootcamp/react/jsx/src/index.js
+++ complete_2023_web_development_bootcamp/react/jsx/src/index.js
@@ -6,13 +6,13 @@ const name = "whzd"
 
 ReactDOM.render(
   <div>
-    <h1>Hello {`${title} ${name}`}!</h1>
+    <h1 className="heading" contentEditable="true" spellCheck="false">Hello {`${title} ${name}`}!</h1>
     <p>Your lucky number is {Math.floor(Math.random() * 20 + 1)}.</p>
-    <ul>
-      <li>element1</li>
-      <li>element2</li>
-      <li>element3</li>
-    </ul>
+    <div>
+      <img src="https://static.wikia.nocookie.net/howrse/images/1/14/5th_element_water.png/revision/latest?cb=20220814144649"/>
+      <img src="https://static.wikia.nocookie.net/howrse/images/9/93/5th_element_fire.png/revision/latest?cb=20220814144645"/>
+      <img src="https://static.wikia.nocookie.net/howrse/images/8/86/5th_element_earth.png/revision/latest?cb=20220814144644"/>
+    </div>
     <p> Created by {name}.</p>
     <p> Copyright {new Date().getFullYear()}.</p>
   </div>,
