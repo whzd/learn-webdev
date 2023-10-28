de48e4c8a5eca8d4db57bab539baa9eb3bc7d1e4
whzd <whzdxl@gmail.com>
2023-10-28 17:30:10 +0100

    mapping data to components

--- complete_2023_web_development_bootcamp/react/props/src/components/App.jsx
+++ complete_2023_web_development_bootcamp/react/props/src/components/App.jsx
@@ -2,32 +2,24 @@ import React from "react"
 import Card from "./Card"
 import { contacts } from "../contacts"
 
-const beyonce = contacts[0]
-const jackBauer = contacts[1]
-const chuckNorris = contacts[2]
+
+function createCard(contact) {
+  return(
+    <Card 
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  )
+}
 
 function App() {
   return(
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
-        name={beyonce.name}
-        img={beyonce.imgURL}
-        tel={beyonce.phone}
-        email={beyonce.email}
-      />
-      <Card
-        name={jackBauer.name}
-        img={jackBauer.imgURL}
-        tel={jackBauer.phone}
-        email={jackBauer.email}
-      />
-      <Card
-        name={chuckNorris.name}
-        img={chuckNorris.imgURL}
-        tel={chuckNorris.phone}
-        email={chuckNorris.email}
-      />
+      {contacts.map(createCard)}
     </div>
   )
 }
